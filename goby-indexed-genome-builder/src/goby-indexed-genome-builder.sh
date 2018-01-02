7ecf689614c962f48a6d300a619fa4e6882d8502
Manuele Simi <manuele.simi@campagnelab.org>
2018-01-02 11:26:58 -0500

    Ignore the errors if the moving of goby files fails.

--- goby-indexed-genome-builder/src/goby-indexed-genome-builder.sh
+++ goby-indexed-genome-builder/src/goby-indexed-genome-builder.sh
@@ -51,10 +51,10 @@ EOL
         bash -c "source ~/.bashrc; cd /out/Goby_Genome; /input/scripts/index.sh"
         
     # the goby genome files are created in the input folder not in the working dir, can we fix this?
-    mv /input/*.sizes /out/Goby_Genome/
-    mv /input/*.bases /out/Goby_Genome/
-    mv /input/*.ignore /out/Goby_Genome/
-    mv /input/*.names /out/Goby_Genome/
+    mv /input/FASTA_Genome/*.sizes /out/Goby_Genome/ || true
+    mv /input/FASTA_Genome/*.bases /out/Goby_Genome/ || true
+    mv /input/FASTA_Genome/*.ignore /out/Goby_Genome/ || true
+    mv /input/FASTA_Genome/*.names /out/Goby_Genome/ || true
     
     dx-upload-all-outputs
 }
