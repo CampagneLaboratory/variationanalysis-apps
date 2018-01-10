b80446acf272a386b8ac0fae98bb2effbef0ccad
Manuele Simi <manuele.simi@campagnelab.org>
2018-01-10 14:29:37 -0500

    Build goby indexer genome to bull the org instead of the developer.

--- build-all-apps.sh
+++ build-all-apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./build-app.sh variation-analysis
 ./build-app.sh bam-to-goby
-./build-app.sh goby-indexed-genome-builder
+./build-app.sh goby-indexed-genome-builder --bill-to org-campagnelab
 ./build-app.sh parallel-gatk-realigner
 ./build-app.sh prepare-sbi-training-set
