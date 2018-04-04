eb09e11710255e9fb8fe512a0e8cb4d4378a7d72
Manuele Simi <manuele.simi@campagnelab.org>
2018-04-04 11:24:58 -0400

    Refactor the app to use a single configuration file. Flatten the inputs in their root dir.

--- prepare-sbi-unlabeled/src/prepare-sbi-unlabeled.sh
+++ prepare-sbi-unlabeled/src/prepare-sbi-unlabeled.sh
@@ -18,44 +18,52 @@
 main() {
 
     dx-download-all-inputs --parallel
-
-    dx-docker pull artifacts/variationanalysis-app:${Image_Version} &>/dev/null
-
+    CONFIG_FILE="${HOME}/in/scripts/configure.sh"
+    mkdir -p ${HOME}/in/scripts/
+    mkdir -p ${HOME}/out/SBI
+    set -x 
+    #flatten the inputs in a single folder
+    find ${HOME}/in/Genome/ -type f -name "${Genome_prefix[0]}.*" -execdir mv {} .. \;
+    find ${HOME}/in/Goby_Alignment/ -type f -name "${Goby_Alignment_prefix[0]}.*" -execdir mv {} .. \;
+    
     # configure
-    genome_basename=`basename /input/indexed_genome/*.bases | cut -d. -f1`
-    echo "export SBI_GENOME=/input/indexed_genome/${genome_basename}" >> /input/configure.sh
-    alignment_basename=`basename /input/alignment/*.entries | cut -d. -f1`
-    echo "export GOBY_ALIGNMENT=/input/alignment/${alignment_basename}" >> /input/configure.sh
-    echo "export GOBY_NUM_SLICES=1" >> /input/configure.sh
+    echo "export SBI_GENOME=/in/Genome/${Genome_prefix[0]}" >> ${CONFIG_FILE}
+    echo "export GOBY_ALIGNMENT=/in/Goby_Alignment/${Goby_Alignment_prefix[0]}" >> ${CONFIG_FILE}
+    echo "export GOBY_NUM_SLICES=1" >> ${CONFIG_FILE}
     # adjust num threads to match number of cores -1:
     cpus=`grep physical  /proc/cpuinfo |grep id|wc -l`
     cpus=`echo $(( cpus / 3 * 2  ))`
-    basename="${alignment_basename}"
-    echo "export SBI_NUM_THREADS=${cpus}" >> /input/configure.sh
-    echo "export INCLUDE_INDELS='true'" >> /input/configure.sh
-    echo "export REALIGN_AROUND_INDELS='false'" >> /input/configure.sh
-    echo "export REF_SAMPLING_RATE='0.01'" >> /input/configure.sh
-    echo "export OUTPUT_PREFIX=${basename}" >> /input/configure.sh
-    echo "export DATASET=${basename}" >> /input/configure.sh
-    echo "export GOBY_NUM_SLICES='50'" >> /input/configure.sh
-    echo "export DO_CONCAT='true'" >> /input/configure.sh
-   
-    cat /input/configure.sh
+    alignment_basename="${Goby_Alignment_prefix[0]}"
+    echo "export SBI_NUM_THREADS=${cpus}" >> ${CONFIG_FILE}
+    echo "export INCLUDE_INDELS='true'" >> ${CONFIG_FILE}
+    echo "export REALIGN_AROUND_INDELS='false'" >> ${CONFIG_FILE}
+    echo "export REF_SAMPLING_RATE='0.01'" >> ${CONFIG_FILE}
+    echo "export OUTPUT_PREFIX=${alignment_basename}" >> ${CONFIG_FILE}
+    echo "export DATASET=${alignment_basename}" >> ${CONFIG_FILE}
+    echo "export GOBY_NUM_SLICES='50'" >> ${CONFIG_FILE}
+    echo "export DO_CONCAT='true'" >> ${CONFIG_FILE}
 
-    # Run generate-genotype-sets-0.02.sh
+    echo "Content of configure.sh: "
+    cat ${CONFIG_FILE}
+    cat >${HOME}/in/run.sh <<EOL
+        #!/bin/bash
+        source /in/scripts/configure.sh
+        cd /out/SBI
+        generate-genotype-unlabeled.sh 20g "/in/Goby_Alignment/${Goby_Alignment_prefix[0]}" "/in/Genome/${Genome_prefix[0]}"
+EOL
+    chmod a+x  ${HOME}/in/run.sh
+
+    echo "Downloading the docker image..."
+    dx-docker pull artifacts/variationanalysis-app:${Image_Version} &>/dev/null
+
     dx-docker run \
-        -v /input/:/input \
-        -v /output/sbi:/output/sbi \
+      -v ${HOME}/out/:/out/ \
+        -v ${HOME}/in/:/in/ \
         artifacts/variationanalysis-app:${Image_Version} \
-        bash -c "source ~/.bashrc; source /input/configure.sh; cd /output/sbi; generate-genotype-unlabeled.sh 20g \"/input/alignment/${alignment_basename}\" \"/input/indexed_genome/${genome_basename}\" "
+        bash -c "source ~/.bashrc; source ~/.bashrc; /in/run.sh "
 
-    ls -lrt /output/sbi
+    ls -lrt $HOME/out/SBI
 
-    # Arrange dataset in output directory and upload:
-    mkdir -p $HOME/out/SBI
-    mv /output/sbi/${basename}*-train.sbi* $HOME/out/SBI/ || true
-    mv /output/sbi/${basename}*-validation.sbi* $HOME/out/SBI/ || true
-    mv /output/sbi/${basename}*-test.sbi* $HOME/out/SBI/  || true
-    ls -lrt $HOME/out/SBI/
+    dx-upload-all-outputs --parallel
 
 }
