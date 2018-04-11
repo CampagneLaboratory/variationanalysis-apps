410c902f3c65b39097f390f7891c4fd779d2196e
Manuele Simi <manuele.simi@campagnelab.org>
2018-04-11 10:25:38 -0400

    Generate VCFs with predict-genotypes.sh.

--- genotype-pytorch-tensors/resources/input/scripts/working_script.sh
+++ genotype-pytorch-tensors/resources/input/scripts/working_script.sh
@@ -13,6 +13,7 @@ function execute {
     mkdir -p /out/sbi
     mkdir -p /out/sbi-vec
     mkdir -p /out/vec-vec
+    mkdir -p /out/vec-vcf
 
     #clone
     export PATH="${HOME}/GenotypeTensors/bin:${PATH}"
@@ -22,10 +23,10 @@ function execute {
 
 
     cd ${MODEL_ARCHIVE_PATH}
-    tar -zxvf ${MODEL_ARCHIVE_FILE}
+    #tar -zxvf ${MODEL_ARCHIVE_FILE}
     #rm ${MODEL_ARCHIVE_FILE}
-    MODEL_DIR=`find ${MODEL_ARCHIVE_PATH} -name  Model_* -type d`
-    MODEL_PATH="${MODEL_ARCHIVE_PATH}/${MODEL_DIR}"
+    export MODEL_PATH=`find ${MODEL_ARCHIVE_PATH}/* -name  Model_* -type d`
+    #MODEL_PATH="${MODEL_ARCHIVE_PATH}/${MODEL_DIR}"
     DOMAIN_PROPERTIES="${MODEL_PATH}/models/domain.properties"
     CHECKPOINT_PROPERTIES="${MODEL_PATH}/models/checkpoint.properties"
 
@@ -38,7 +39,7 @@ function execute {
     GenomicContextLength=`cat ${DOMAIN_PROPERTIES} | grep stats.genomicContextSize.max | cut -d= -f2`
     ExtraGenotypes=`cat ${DOMAIN_PROPERTIES} | grep extraGenotypes | cut -d= -f2`
     CHECKPOINT_KEY=`cat ${CHECKPOINT_PROPERTIES} | grep default.checkpoint | cut -d= -f2`
-
+    rm -f /out/sbi-vec/commands.txt
     cd /out/sbi-vec
     for file in /out/sbi/out-part*.sbi; do
         SBI_basename=`basename /out/sbi/$file .sbi`
@@ -46,16 +47,26 @@ function execute {
         if [ ! -f /out/sbi/${SBI_basename}.sbip ]; then
           echo "${SBI_basename}.sbip not found!"
         fi
+        OUTPUT_VEC="${DATASET_BASENAME}_${DATASET_NAME}_${CHECKPOINT_KEY}_${SBI_basename}_predicted"
+
+        if [ -z "${MINI_BATCH_SIZE+set}" ]; then
+            MINI_BATCH_SIZE="2048"
+            echo "MINI_BATCH_SIZE set to ${MINI_BATCH_SIZE}. Change the variable to switch the mini-batch-size."
+        fi
+
         echo "cd /out/sbi-vec && export-genotype-tensors.sh 2g --indel-sequence-length ${IndelSequenceLength} --feature-mapper ${FeatureMapper} \
         -i \"/out/sbi/${SBI_basename}.sbi\" -o /out/sbi-vec/${SBI_basename}-${DATASET_NAME} --label-smoothing-epsilon ${LabelSmoothingEpsilon} \
         --ploidy ${Ploidy} --genomic-context-length ${GenomicContextLength} --export-input input --export-output softmaxGenotype \
         --export-output metaData --extra-genotypes ${ExtraGenotypes} --sample-name \"${SAMPLE_NAME}\"  --sample-type germline \
-        && cd /out/vec-vec && predict-genotypes-pytorch.sh 10g \"${MODEL_PATH}\ \"${MODEL_NAME}\" /out/sbi-vec/${SBI_basename}-${DATASET_NAME}" >> /out/sbi-vec/commands.txt
+        && cd /out/vec-vec && predict-genotypes-pytorch.sh 10g \"${MODEL_PATH}\" \"${MODEL_NAME}\" ${OUTPUT_VEC} /out/sbi-vec/${SBI_basename} \
+        && cd /out/vec-vcf && predict-genotypes.sh 4g -m ${MODEL_PATH}/models -l ${MODEL_NAME}  --no-cache --mini-batch-size ${MINI_BATCH_SIZE} \
+        --vec-path \"/out/vec-vec/${OUTPUT_VEC}.vec\" -f --format VCF --checkpoint-key ${CHECKPOINT_KEY} \
+        -i \"/out/sbi/${SBI_basename}.sbi\" " >> /out/sbi-vec/commands.txt
 
      done
     cat /out/sbi-vec/commands.txt
     parallel --bar --eta -j${SBI_NUM_THREADS} --plus  --progress :::: /out/sbi-vec/commands.txt
-    
+    cd /out/vec-vcf
     cat ${MODEL}-${MODEL_NAME}-*.vcf | vcf-sort >sorted-${MODEL}-${MODEL_LABEL}.vcf
     bgzip -f sorted-${MODEL}-${MODEL_LABEL}.vcf
     tabix -f sorted-${MODEL}-${MODEL_LABEL}.vcf.gz
